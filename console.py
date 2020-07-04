71cd08af24700505703195d6e6406fa936511a4b
moussaddak <meddeb.moussaddak@gmail.com>
2020-07-04 21:19:09 +0000

    console AirBnB

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Console Implementation """
+from cmd import Cmd
+
+
+class HBNBCommand(Cmd):
+    """ command interpreter methods """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """ exit the program """
+        return True
+
+    def do_EOF(self, line):
+        """ exit the program """
+        return True
+
+    def emptyline(self):
+        """ empty line """
+        pass
+
+
+
+if __name__ == "__main__":
+    ''' Entry point for the loop '''
+    HBNBCommand().cmdloop()
