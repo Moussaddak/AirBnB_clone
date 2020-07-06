788050d1f8aca70d929fdabacbd18d1122a68de4
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-07-06 13:51:57 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
+            #models.storage.new(self)
 
     def __str__(self):
         """
@@ -39,7 +39,7 @@ class BaseModel:
     def save(self):
         """ updates attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        #models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary of the instance"""
