a2aa3f0420a47122783f08e1de9e3465c7942474
moussaddak <meddeb.moussaddak@gmail.com>
2020-07-09 00:09:21 +0000

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,6 +4,7 @@
 """
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
@@ -11,7 +12,16 @@ class TestUser(unittest.TestCase):
     def test_isinstance_User(self):
         """test if the data is an type User"""
         data = User()
-        self.assertTrue(isinstance(data, User))
+        self.assertTrue(isinstance(data, BaseModel))
+
+    def test_attributes_User(self):
+        """ Test user attributes """
+
+        data = User()
+        self.assertTrue("email" in data.__dir__())
+        self.assertTrue("first_name" in data.__dir__())
+        self.assertTrue("last_name" in data.__dir__())
+        self.assertTrue("password" in data.__dir__())
 
 
 if __name__ == '__main__':
